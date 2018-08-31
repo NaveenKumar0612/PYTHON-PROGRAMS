f3724ff69d72a05f2d76c8d7a542fa340a755da3
naveensanjay025 <42862879+naveensanjay025@users.noreply.github.com>
2018-08-31 15:18:39 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,10 @@
+def main():
+    a=int(raw_input("enter a number"))
+    if(a>0):
+      print("The number is positive")
+    elif(a<0):
+        print("The number is negative")
+    else:
+        print("The number is zero")
+        
+main()
