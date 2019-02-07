26bf8410a62899bcc2d4fe1b9a7efff43d8b7f00
naveensanjay025 <42862879+naveensanjay025@users.noreply.github.com>
2019-02-07 10:55:25 +0530

    Update even or odd.py

--- even or odd.py
+++ even or odd.py
@@ -1,8 +1,5 @@
-num = int(input("Enter any number: "))
-flag = num%2
-if flag == 0;
-    print(num, "is an even number")
-elif flag == 1;
-    print(num, "is an odd number")
-else;
-    print("Error, Invalid input")
+input_num = int(input('Enter any number: '))
+if input_num % 2 == 0:
+    print(input_num, "is EVEN")
+else:
+    print(input_num, "is ODD")
