e9aa9860da177bcbf8b1cdd6692b27cb7e4094ce
naveensanjay025 <42862879+naveensanjay025@users.noreply.github.com>
2019-02-07 14:04:46 +0530

    Update greaterno.py

--- greaterno.py
+++ greaterno.py
@@ -1,11 +1,11 @@
-c=int(input())
-b=int(input())
 a=int(input())
-if(c>b and c>a):
-  print(c)
+b=int(input())
+c=int(input())
+if(a>b and a>b):
+  print(a)
 elif(b>a and b>c):
   print(b)
-elif(a>c and a>b):
-  print(a)
+elif(c>a and c>b):
+  print(c)
 else:
   print("INVALID")
