2031fc2d0ba91adf168c4b4025a9ccab605d14ec
naveensanjay025 <42862879+naveensanjay025@users.noreply.github.com>
2019-02-08 10:55:03 +0530

    Update powern.py

--- powern.py
+++ powern.py
@@ -1,9 +1,6 @@
-def power(x,y):
-
-    if(y==0): return 1
-    elif(int(y%2)==0):
-        return(power(x,int(y/2))*
-              power(x,int(y/2))
-    else:
-        return(x*power(x,int(y/2))*
-                 (power(x,int(y/2))
+#power of a number
+c=input().split()
+n=int(c[0])
+p=int(c[1])
+pow=n**p
+print(pow)
