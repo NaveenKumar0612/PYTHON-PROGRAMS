2bae8684a87fb062ee58eb33152cc98e2c832c61
naveensanjay025 <42862879+naveensanjay025@users.noreply.github.com>
2019-02-08 11:10:45 +0530

    Update palin.py

--- palin.py
+++ palin.py
@@ -1,10 +1,10 @@
 num = int(input())
 temp = num
 rev = 0
-while temp ! = 0:
-    rev = (rev * 10) + (temp % 10)
-    temp = temp//10
+while temp != 0:
+        rev = (rev * 10) + (temp % 10)
+        temp = temp//10
 if num == rev:
-    print("yes")
+        print("yes")
 else:
-    print("no")
+        print("no")
