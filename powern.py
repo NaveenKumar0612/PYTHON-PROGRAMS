71175c7ad1b7d7249acd2e7acacfa3fe2312db2f
naveensanjay025 <42862879+naveensanjay025@users.noreply.github.com>
2019-02-08 10:41:19 +0530

    Create powern.py

---
+++ powern.py
@@ -0,0 +1,9 @@
+def power(x,y):
+
+    if(y==0): return 1
+    elif(int(y%2)==0):
+        return(power(x,int(y/2))*
+              power(x,int(y/2))
+    else:
+        return(x*power(x,int(y/2))*
+                 (power(x,int(y/2))
