960d11a918f058580afb804802fa3b7fa45b8a7a
naveensanjay025 <42862879+naveensanjay025@users.noreply.github.com>
2019-02-08 14:34:47 +0530

    Update arm.py

--- arm.py
+++ arm.py
@@ -1,8 +1,8 @@
 #ArmStrong
 n=int(input())
-a=list(map(int,str(n)))
-b=list(map(lambda x:x**3,a))
-if(sum(b)==n):
+c=list(map(int,str(n)))
+d=list(map(lambda x:x**3,c))
+if(sum(d)==n):
     print("yes")
 else:
     print("no")
