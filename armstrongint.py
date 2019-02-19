05c9e5f17123163ceb0aaf1720aec18f2a224f5c
naveensanjay025 <42862879+naveensanjay025@users.noreply.github.com>
2019-02-19 12:30:08 +0530

    Create armstrongint.py

---
+++ armstrongint.py
@@ -0,0 +1,12 @@
+n=input().split()
+p=int(n[0])
+q=int(n[1])
+for num in range(p,q):
+  temp=sum
+  sum=0
+  while temp>0;
+    digit=temp%10
+    sum=sum+digit**3
+    temp=temp//10
+    if sum==num;
+      print(sum)
