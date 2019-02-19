cf22e82db7ce16effe2b018f87f5b6fed553b60e
naveensanjay025 <42862879+naveensanjay025@users.noreply.github.com>
2019-02-19 13:39:51 +0530

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,11 @@
+#factorial
+num=int(input())
+factorial = 1
+if num < 0:
+   print("Factorial does not exist for negative numbers")
+elif num == 0:
+   print("1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print(factorial)
